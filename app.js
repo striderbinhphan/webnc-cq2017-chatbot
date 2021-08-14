c621e5419aa5e8b540e54cb675f99654c9211059
Steven <steven999cass@gmail.com>
2021-08-15 00:01:35 +0700

    fix

--- app.js
+++ app.js
@@ -114,42 +114,42 @@ function handleMessage(senderPsid, receivedMessage) {
       'text': 'Test'
     };
   }
-  if (receivedMessage.text) {
-    // Create the payload for a basic text message, which
-    // will be added to the body of your request to the Send API
-    response = {
-      'text': `You sent the message: '${receivedMessage.text}'. Now send me an attachment!`
-    };
-  } else if (receivedMessage.attachments) {
+  // if (receivedMessage.text) {
+  //   // Create the payload for a basic text message, which
+  //   // will be added to the body of your request to the Send API
+  //   response = {
+  //     'text': `You sent the message: '${receivedMessage.text}'. Now send me an attachment!`
+  //   };
+  // } else if (receivedMessage.attachments) {
 
-    // Get the URL of the message attachment
-    let attachmentUrl = receivedMessage.attachments[0].payload.url;
-    response = {
-      'attachment': {
-        'type': 'template',
-        'payload': {
-          'template_type': 'generic',
-          'elements': [{
-            'title': 'Is this the right picture?',
-            'subtitle': 'Tap a button to answer.',
-            'image_url': attachmentUrl,
-            'buttons': [
-              {
-                'type': 'postback',
-                'title': 'Yes!',
-                'payload': 'yes',
-              },
-              {
-                'type': 'postback',
-                'title': 'No!',
-                'payload': 'no',
-              }
-            ],
-          }]
-        }
-      }
-    };
-  }
+  //   // Get the URL of the message attachment
+  //   let attachmentUrl = receivedMessage.attachments[0].payload.url;
+  //   response = {
+  //     'attachment': {
+  //       'type': 'template',
+  //       'payload': {
+  //         'template_type': 'generic',
+  //         'elements': [{
+  //           'title': 'Is this the right picture?',
+  //           'subtitle': 'Tap a button to answer.',
+  //           'image_url': attachmentUrl,
+  //           'buttons': [
+  //             {
+  //               'type': 'postback',
+  //               'title': 'Yes!',
+  //               'payload': 'yes',
+  //             },
+  //             {
+  //               'type': 'postback',
+  //               'title': 'No!',
+  //               'payload': 'no',
+  //             }
+  //           ],
+  //         }]
+  //       }
+  //     }
+  //   };
+  //}
 
   // Send the response message
   callSendAPI(senderPsid, response);
