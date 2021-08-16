6047582458c424e4875078e7083af6d93066e606
Steven <steven999cass@gmail.com>
2021-08-16 16:13:25 +0700

    handle view course detail test 5

--- app.js
+++ app.js
@@ -252,97 +252,71 @@ async function handlePostback(senderPsid, receivedPostback) {
   if(payload.includes("course_id_")){
     const res = await  axios.get(`https://onlinecourse-be.herokuapp.com/courses/${payload[payload.length-1]}`);
     console.log(`category id = ${payload[payload.length-1]} data`,res.data);
-    response = 
-    {
-      'attachment': {
-        'type': 'template',
-        'payload': {
-          'template_type': 'list',
-          'elements': [{
-            'title': 'Is this the right picture?',
-            'subtitle': 'Tap a button to answer.',
-            'image_url': attachmentUrl,
-            'buttons': [
-              {
-                'type': 'postback',
-                'title': 'Yes!',
-                'payload': 'yes',
-              },
-              {
-                'type': 'postback',
-                'title': 'No!',
-                'payload': 'no',
+    response = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "top_element_style": "compact",
+          "elements": [
+            {
+              "title": "Classic T-Shirt Collection",
+              "subtitle": "See all our colors",
+              "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",          
+              "buttons": [
+                {
+                  "title": "View",
+                  "type": "web_url",
+                  "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                  "messenger_extensions": true,
+                  "webview_height_ratio": "tall",
+                  "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+                }
+              ]
+            },
+            {
+              "title": "Classic White T-Shirt",
+              "subtitle": "See all our colors",
+              "default_action": {
+                "type": "web_url",
+                "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                "messenger_extensions": false,
+                "webview_height_ratio": "tall"
               }
-            ],
-          }]
+            },
+            {
+              "title": "Classic Blue T-Shirt",
+              "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+              "subtitle": "100% Cotton, 200% Comfortable",
+              "default_action": {
+                "type": "web_url",
+                "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                "messenger_extensions": true,
+                "webview_height_ratio": "tall",
+                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+              },
+              "buttons": [
+                {
+                  "title": "Shop Now",
+                  "type": "web_url",
+                  "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                  "messenger_extensions": true,
+                  "webview_height_ratio": "tall",
+                  "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+                }
+              ]        
+            }
+          ],
+           "buttons": [
+            {
+              "title": "View More",
+              "type": "postback",
+              "payload": "payload"            
+            }
+          ]  
         }
       }
-    };
-    // {
-    //   "attachment": {
-    //     "type": "template",
-    //     "payload": {
-    //       "template_type": "list",
-    //       "top_element_style": "compact",
-    //       "elements": [
-            // {
-            //   "title": "Classic T-Shirt Collection",
-            //   "subtitle": "See all our colors",
-            //   "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",          
-            //   "buttons": [
-            //     {
-            //       "title": "View",
-            //       "type": "web_url",
-            //       "url": "https://peterssendreceiveapp.ngrok.io/collection",
-            //       "messenger_extensions": true,
-            //       "webview_height_ratio": "tall",
-            //       "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
-            //     }
-            //   ]
-            // },
-            // {
-            //   "title": "Classic White T-Shirt",
-            //   "subtitle": "See all our colors",
-            //   "default_action": {
-            //     "type": "web_url",
-            //     "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
-            //     "messenger_extensions": false,
-            //     "webview_height_ratio": "tall"
-            //   }
-            // },
-            // {
-            //   "title": "Classic Blue T-Shirt",
-            //   "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
-            //   "subtitle": "100% Cotton, 200% Comfortable",
-            //   "default_action": {
-            //     "type": "web_url",
-            //     "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
-            //     "messenger_extensions": true,
-            //     "webview_height_ratio": "tall",
-            //     "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-            //   },
-            //   "buttons": [
-            //     {
-            //       "title": "Shop Now",
-            //       "type": "web_url",
-            //       "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
-            //       "messenger_extensions": true,
-            //       "webview_height_ratio": "tall",
-            //       "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
-            //     }
-            //   ]        
-            // }
-    //       ],
-    //        "buttons": [
-    //         {
-    //           "title": "View More",
-    //           "type": "postback",
-    //           "payload": "payload"            
-    //         }
-    //       ]  
-    //     }
-    //   }
-    // }//end response
+    }//end response
     console.log("response dta", response);
 
   }//end if course id view
