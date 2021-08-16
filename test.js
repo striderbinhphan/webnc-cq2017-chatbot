e349f636add7721918912228c13900cfabedddad
Steven <steven999cass@gmail.com>
2021-08-16 15:54:04 +0700

    handle view course detail test 1

--- test.js
+++ test.js
@@ -16,7 +16,7 @@ async function load(){
 
 // load()
 
-async function load2(){
+async function getCourseByCategoryId(){
     const res = await  axios.get(`https://onlinecourse-be.herokuapp.com/courses/category/1`);
       response = {
         'attachment': {
@@ -41,4 +41,30 @@ async function load2(){
     console.log(response.attachment.payload.elements[0].buttons)
 
 }
-load2()
+
+async function getCourseDetail(){
+  const res = await  axios.get(`https://onlinecourse-be.herokuapp.com/courses/1`);
+    // response = {
+    //   'attachment': {
+    //     'type': 'template',
+    //     'payload': {
+    //       'template_type': 'generic',
+    //       'elements': res.data.map(course=>(
+    //         {
+    //         'title': `${course.course_name}`,
+    //         'image_url': `${course.course_image}`,
+    //         'buttons': [
+    //           {
+    //             'type': 'postback',
+    //             'title': 'xem chi tiết',
+    //             'payload': `${course.course_name}`,
+    //           }]
+    //         }))
+    //     }
+    //  }
+    // }
+    console.log(res.data);
+  console.log("response.attachment.payload.elements[0].buttons")
+
+}
+getCourseDetail()
